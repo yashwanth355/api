35b377a4cf23e9ad861475724356b54a21c8a8a9
Yashwanth <ykotha@dginosolutions.com>
2022-01-25 14:56:38 +0530

    commit

--- variables.tf
+++ variables.tf
@@ -1,10 +1,15 @@
 variable "aws_region" {
   default     = "ap-south-1"
-  description = "aws region where our resources going to create choose"
+  #description = "aws region where our resources going to create choose"
   #replace the region as suits for your requirement
 }
 
 variable "az_count" {
   default     = "2"
-  description = "number of availability zones in above region"
+  #description = "number of availability zones in above region"
+}
+
+variable "ecs_task_execution_role" {
+  default     = "myECcsTaskExecutionRole"
+  description = "ECS task execution role name"
 }
--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "test-vpc" {
+resource "aws_vpc" "dev-vpc" {
   cidr_block = "172.16.0.0/16"
 }
 
@@ -12,38 +12,38 @@ resource "aws_subnet" "private" {
   count             = var.az_count
   cidr_block        = cidrsubnet(aws_vpc.test-vpc.cidr_block, 8, count.index)
   availability_zone = data.aws_availability_zones.available.names[count.index]
-  vpc_id            = aws_vpc.test-vpc.id
+  vpc_id            = aws_vpc.dev-vpc.id
 }
 
 # Create var.az_count public subnets, each in a different AZ
 resource "aws_subnet" "public" {
   count                   = var.az_count
-  cidr_block              = cidrsubnet(aws_vpc.test-vpc.cidr_block, 8, var.az_count + count.index)
+  cidr_block              = cidrsubnet(aws_vpc.dev-vpc.cidr_block, 8, var.az_count + count.index)
   availability_zone       = data.aws_availability_zones.available.names[count.index]
-  vpc_id                  = aws_vpc.test-vpc.id
+  vpc_id                  = aws_vpc.dev-vpc.id
   map_public_ip_on_launch = true
 }
 
 # Internet Gateway for the public subnet
-resource "aws_internet_gateway" "test-igw" {
-  vpc_id = aws_vpc.test-vpc.id
+resource "aws_internet_gateway" "dev-igw" {
+  vpc_id = aws_vpc.dev-vpc.id
 }
 
 # Route the public subnet traffic through the IGW
 resource "aws_route" "internet_access" {
-  route_table_id         = aws_vpc.test-vpc.main_route_table_id
+  route_table_id         = aws_vpc.dev-vpc.main_route_table_id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.test-igw.id
+  gateway_id             = aws_internet_gateway.dev-igw.id
 }
 
 # Create a NAT gateway with an Elastic IP for each private subnet to get internet connectivity
-resource "aws_eip" "test-eip" {
+resource "aws_eip" "dev-eip" {
   count      = var.az_count
   vpc        = true
-  depends_on = [aws_internet_gateway.test-igw]
+  depends_on = [aws_internet_gateway.dev-igw]
 }
 
-resource "aws_nat_gateway" "test-natgw" {
+resource "aws_nat_gateway" "dev-natgw" {
   count         = var.az_count
   subnet_id     = element(aws_subnet.public.*.id, count.index)
   allocation_id = element(aws_eip.test-eip.*.id, count.index)
@@ -52,11 +52,11 @@ resource "aws_nat_gateway" "test-natgw" {
 # Create a new route table for the private subnets, make it route non-local traffic through the NAT gateway to the internet
 resource "aws_route_table" "private" {
   count  = var.az_count
-  vpc_id = aws_vpc.test-vpc.id
+  vpc_id = aws_vpc.dev-vpc.id
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = element(aws_nat_gateway.test-natgw.*.id, count.index)
+    nat_gateway_id = element(aws_nat_gateway.dev-natgw.*.id, count.index)
   }
 }
 
