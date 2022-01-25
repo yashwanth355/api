360988d5b3cf052a907e2e801b701145a8c027e7
Yashwanth <ykotha@dginosolutions.com>
2022-01-25 14:19:10 +0530

    commit

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" {
-  default     = "eu-west-1"
+  default     = "ap-south-1"
   description = "aws region where our resources going to create choose"
   #replace the region as suits for your requirement
 }
