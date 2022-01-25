0ca2b791756ea84c74e2689bbf8189664147346e
Yashwanth <ykotha@dginosolutions.com>
2022-01-25 14:17:58 +0530

    commit

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  default     = "eu-west-1"
+  description = "aws region where our resources going to create choose"
+  #replace the region as suits for your requirement
+}
+
+variable "az_count" {
+  default     = "2"
+  description = "number of availability zones in above region"
+}
