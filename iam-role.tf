83c2c414bef87564d7e209f064554a4477bb29e7
Yashwanth <ykotha@dginosolutions.com>
2022-01-27 10:54:19 +0530

    updated

--- iam-role.tf
+++ iam-role.tf
@@ -7,10 +7,10 @@ data "aws_iam_policy_document" "ecs_task_execution_role" {
 
     principals {
       type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-      identifiers = ["ec2-tasks.amazonaws.com"]
-      identifiers = ["s3-tasks.amazonaws.com"]
-      identifiers = ["lambda-tasks.amazonaws.com"]
+      identifiers = ["ecs-tasks.amazonaws.com"],
+      identifiers = ["ec2-tasks.amazonaws.com"],
+      identifiers = ["s3-tasks.amazonaws.com"],
+      identifiers = ["lambda-tasks.amazonaws.com"],
 
     }
   }
