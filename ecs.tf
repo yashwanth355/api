f17d31e555cf3377364446348cda1763453bdc94
Yashwanth <ykotha@dginosolutions.com>
2022-02-01 19:55:40 +0530

    Merge branch 'main' of https://github.com/yashwanth355/leapfrog-terraform-aws-ecs

--- ecs.tf
+++ ecs.tf
@@ -1,13 +1,44 @@
-#resource "aws_ecs_cluster" "ecs-cluster" {
-#  name               = var.cluster_name
-# capacity_providers = [aws_ecs_capacity_provider.test.name]
-#  tags = {
-#    "env"       = "dev"
-#
-#  }
-#}
-#
+##########################################################
+# AWS ECS-CLUSTER
+#########################################################
 
+resource "aws_ecs_cluster" "cluster" {
+  name = "ecs-devl-cluster"
+  tags = {
+   name = ecs-cluster-name
+   }
+   
+  }
+  
+###########################################################
+# AWS ECS-EC2
+###########################################################
+resource "aws_instance" "ec2_instance" {
+  ami                    = "ami-08935252a36e25f85"
+  subnet_id              =  "subnet-087e48d4db31e442d" #CHANGE THIS
+  instance_type          = "t2.medium"
+  iam_instance_profile   = "ecsInstanceRole" #CHANGE THIS
+  vpc_security_group_ids = ["sg-01849003c4f9203ca"] #CHANGE THIS
+  key_name               = "pnl-test" #CHANGE THIS
+  ebs_optimized          = "false"
+  source_dest_check      = "false"
+  user_data              = "${data.template_file.user_data.rendered}"
+  root_block_device = {
+    volume_type           = "gp2"
+    volume_size           = "30"
+    delete_on_termination = "true"
+  }
 
+  tags {
+    Name                   = "openapi-ecs-ec2_instance"
+}
 
+  lifecycle {
+    ignore_changes         = ["ami", "user_data", "subnet_id", "key_name", "ebs_optimized", "private_ip"]
+  }
+}
+
+data "template_file" "user_data" {
+  template = "${file("${path.module}/user_data.tpl")}"
+}
 
